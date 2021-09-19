51cea62347412a7378cfba642e68cafd92b42dc0
mariabambara <mariafbambara@gmail.com>
2021-09-19 17:32:00 -0400

    Added real life weather icon

--- script.js
+++ script.js
@@ -60,6 +60,12 @@ function displayTemperature(response) {
     response.data.main.temp
   );
   descriptionElement.innerHTML = response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchInput(city) {
